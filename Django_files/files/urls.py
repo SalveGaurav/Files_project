43eb849b1a990932fcb77aa9412be8e073ba3a4b
SalveGaurav <64093120+SalveGaurav@users.noreply.github.com>
2020-10-13 12:38:12 +0530

    Update urls.py

--- Django_files/files/urls.py
+++ Django_files/files/urls.py
@@ -21,5 +21,5 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.index,name="index"),
-    path('upload', views.upload,name="upload")
+    path('upload/', views.upload,name="upload")
 ]+ static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
