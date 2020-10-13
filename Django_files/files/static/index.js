e710162dce23e7f81560e6c953a04032930fd93a
SalveGaurav <64093120+SalveGaurav@users.noreply.github.com>
2020-10-13 12:35:31 +0530

    Update index.js

--- Django_files/files/static/index.js
+++ Django_files/files/static/index.js
@@ -1,6 +1,6 @@
 $(document).ready(function(){
     $('#Send').click(function(){
-        const Ipfiles=document.getElementById("files");
+       /* const Ipfiles=document.getElementById("files");
         console.log(Ipfiles.files)
         const button=document.getElementById("Send");
         //const xhr=new XMLHttpRequest();
@@ -23,7 +23,40 @@ $(document).ready(function(){
               
                  alert("sucessful")
                }
+        });*/
+        var data = new FormData();
+        $.each($("#files")[0].files, function(i, file){
+            data.append("file", file);
         });
-    
+        data.append("csrfmiddlewaretoken", $('input[name=csrfmiddlewaretoken]').val());
+        console.log(data);        
+        $.ajax({
+            type: "POST",
+            url: "/upload/",  
+            data:data,
+            processData: false,
+            contentType: false,
+            success:  function(data){
+                alert(data);
+                
+                data = JSON.parse(data);
+                console.log(data)
+                
+                for(var i in data){
+                    console.log(data[i]['file']);
+                    var newLine=document.createElement('br');
+                    var aTag = document.createElement('a');
+                    var loc="/media/"+data[i]['file']
+                    aTag.setAttribute('href',loc);
+                    aTag.setAttribute('download',"download")
+                    var fileName=data[i]['file'];
+                    var fileName=fileName.split("/")
+                    aTag.innerText = fileName[1];
+                    $("#right").append(aTag);
+                    $("#right").append(newLine);
+                }         
+                               
+       }
+        });
     });
     });
